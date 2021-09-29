b4ff0fe4705265261734841f6febf266ca2c65ed
Alvin Saini <70733646+CodeWithAlvin@users.noreply.github.com>
2021-09-29 18:30:15 +0530

    some work remaning

--- main.py
+++ main.py
@@ -1,170 +1,49 @@
-##imports ##
-import os,sys
+import os
+import sys
 
+# defining extensions
+img=[".jpg",".png",".jpeg"]
+docs=[".txt",".doc",".docx",".csv",".xlsx",".md"]
+pdf=[".pdf"]
+video=[".mp4",".avi",".mkv",".3gp",".mkv"]
+audio=[".mp3",".flv",".mpeg",".ogg",".wav"]
 
 
-#getting cwd
+def move(file,to):
+    pass
 
-cwd=os.getcwd()
+def get_extension(file):
+    return os.path.splitext(file)[-1]
 
-print(f"folder : {cwd}\n\n")
+def create_dir(directory,dirs):
+    for dir in dirs:
+        if not os.path.exists(os.path.join(directory,dir)):
+            os.mkdir(os.path.join(directory,dir))
 
+path = input("Enter the apth to organize: ")
 
+if not os.path.exists(path):
+    print("Path doesn't exist")
+    sys.exit()
 
-#listing files and removing
+items = os.listdir(path)
 
-lis= os.listdir()
+create_dir(['Images','Videos','Docs','Audios','Pdfs','Others'])
 
-if lis==["organizer.py"]:
+for item in items:
+    ext = get_extension(item)
 
- print(f"No files found in folder : {os.getcwd()}")
+    classification = {
+        'Images':img,
+        'Videos':video,
+        'Docs':docs,
+        'Audios':audio
+        ,'Pdfs':pdf,
+    }
 
- sys.exit()
+    for key,value in classification.items():
+        if ext in value:
+            move
 
-lis.remove("organizer.py")
 
-files=lis.copy()
 
-
-
-class main:
-
- def __init__(self):
-  self.path=os.getcwd()
-  
-
- def getext(self,ext_name):
-
-  exts=[fil for fil in files if os.path.splitext(fil)[1].lower() in ext_name]
-
-  return exts
-
-	
-
- def create_if_not(self,folder):
-
-     if not os.path.exists(folder):
-
-      os.makedirs(folder)
-
-	
-
- def move(self,foldername,filee):
-
-      for fil in filee:          
-
-          os.replace(fil,f"{cwd}/{foldername}/{fil}")
-
-    
-
-
-
-
-
-#===creating folders===#
-
-print("=========================================")
-
-print("Defining folder's")
-
-
-
-main.create_if_not("python_Images")
-
-main.create_if_not("python_Docs")
-
-main.create_if_not("python_Pdf")
-
-main.create_if_not("python_Video")
-
-main.create_if_not("python_Audio")
-
-main.create_if_not("python_Others")
-
-
-
-#=====extentions======#
-
-print("=========================================")
-
-print("Getting files extentions")
-
-
-
-img_ext=[".jpg",".png",".jpeg"]
-
-docs_ext=[".txt",".doc",".docx",".csv",".xlsx",".md"]
-
-pdf_ext=[".pdf"]
-
-video_ext=[".mp4",".avi",".mkv",".3gp",".mkv"]
-
-audio_ext=[".mp3",".flv",".mpeg",".ogg",".wav"]
-
-
-
-#=====classificatipn of files=====#
-
-print("=========================================")
-
-print("Classifing files according to extensions")
-
-
-
-images=main.getext(img_ext)
-
-docs=main.getext(docs_ext)
-
-pdf=main.getext(pdf_ext)
-
-video=main.getext(video_ext)
-
-audio=main.getext(audio_ext)
-
-other=[]
-
-
-
-#=====appeding files=====#
-
-for fil in files:
-
-     ext=os.path.splitext(fil)[1].lower()
-
-     if (ext not in img_ext) and (ext not in docs_ext) and (ext not in pdf_ext) and (ext not in video_ext) and (ext not in audio_ext) and os.path.isfile(fil):
-
-         other.append(fil) 
-
-
-
-#====moving files=====#
-
-print("=========================================")
-
-print("Moving files according to extensions")
-
-
-
-main.move("python_Images",images)
-
-main.move("python_Docs",docs)
-
-main.move("python_Pdf",pdf)
-
-main.move("python_Video",video)
-
-main.move("python_Audio",audio)
-
-main.move("python_Others",other)
-
-
-
-
-
-#=====running class======#
-
-print("=========================================")
-
-print("Task Successed")
-#running class
-run=main()
